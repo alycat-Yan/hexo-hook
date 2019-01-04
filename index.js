24994d35c64859066126e5ae3cf6dee943990c7f
alycat-Yan <lucas@alycat.cn>
2019-01-05 03:08:57 +0800

    add todo for develop

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+/* global hexo */
+
 'use strict';
-hexo.extend.console.register('hook', require('./hook'));
+hexo.config.webhook = Object.assign({
+  port: 4000,
+  log: false,
+  // `undefined` uses Node's default (try `::` with fallback to `0.0.0.0`)
+  repository: [{
+    type: 'github',
+    url: undefined,
+    local: 'source/_posts'
+  }],
+  path: undefined,
+  header: true
+}, hexo.config.webhook);
+
+hexo.extend.console.register('hook', 'Start the server.', {
+  desc: 'Start the server and watch for file changes.',
+  options: [
+  ]
+}, require('./lib/hook'));
